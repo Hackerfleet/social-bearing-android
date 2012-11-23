847af750a983c2d6e739ee7a1194619afd0b6ce1
ligi <ligi@ligi.de>
2012-11-23 23:02:57 +0100

    fix prj so it compiles

--- src/org/hackerfleet/BearingActivity.java
+++ src/org/hackerfleet/BearingActivity.java
@@ -1,17 +1,16 @@
 package org.hackerfleet;
-
-import com.actionbarsherlock.app.SherlockActivity;
+import org.holoeverywhere.app.Activity;
 
 import android.os.Bundle;
 
-public class BearingActivity extends SherlockActivity {
+public class BearingActivity extends Activity {
   /**
    * Called when the activity is first created.
    */
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate (savedInstanceState);
-    setContentView (R.layout.main);
+   setContentView (R.layout.main);
 
     getSupportActionBar().setTitle("Corellian Engineering Corporation");
   }
