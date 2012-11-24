b6aa3e211a04e0fd69adad30bdd27080a7e18129
ligi <ligi@ligi.de>
2012-11-24 04:30:16 +0100

    accuracy displaying / updating

--- src/org/hackerfleet/ApplicationController.java
+++ src/org/hackerfleet/ApplicationController.java
@@ -1,6 +1,5 @@
 package org.hackerfleet;
 
-import org.hackerfleet.etc.AppDefs;
 import org.holoeverywhere.app.Application;
 
 import android.content.Context;
@@ -8,18 +7,17 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 
 public class ApplicationController extends Application {
 
 	private Location last_loc;
+	private LocationManager mLocationManager;
 	
 	@Override
 	public void onCreate() {
-		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-		Bundle b;
-		
+		// mainly adding this listener to get first fix fast
 		LocationListener locationListener = new LocationListener() {
 		    public void onLocationChanged(Location location) {
 		    	last_loc=location;
@@ -32,8 +30,7 @@ public class ApplicationController extends Application {
 		    public void onProviderDisabled(String provider) {}
 		  };
 
-		// Register the listener with the Location Manager to receive location updates
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 		
 		super.onCreate();
 	}
@@ -41,5 +38,8 @@ public class ApplicationController extends Application {
 	public Location getLastLocation() {
 		return last_loc;
 	}
-	
+
+	public LocationManager getLocationManager() {
+		return mLocationManager;
+	}
 }
--- src/org/hackerfleet/MeasureStartActivity.java
+++ src/org/hackerfleet/MeasureStartActivity.java
@@ -4,28 +4,55 @@ import org.hackerfleet.etc.AppDefs;
 import org.holoeverywhere.app.Activity;
 import org.holoeverywhere.widget.TextView;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.ImageView;
 
-public class MeasureStartActivity extends Activity {
+public class MeasureStartActivity extends Activity implements LocationListener {
 
 	private Integer buoy;
+	private TextView sat_tv;
+	private ApplicationController ac;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		setContentView (R.layout.measure_start);
-		ApplicationController ac=(ApplicationController)this.getApplicationContext();
+		ac=(ApplicationController)this.getApplicationContext();
 		
 		buoy=getIntent().getExtras().getInt("buoy");
-		TextView sat_tv=(TextView)findViewById(R.id.sat_count);
+		sat_tv=(TextView)findViewById(R.id.sat_count);
 		if (ac.getLastLocation()==null)
 			sat_tv.setText("NO GPS");
 		else
 			sat_tv.setText(""+ac.getLastLocation().getAccuracy()+"m");
 		
+		ac.getLocationManager().requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+		
 		ImageView bouy_icon=(ImageView)findViewById(R.id.buoy_icon);
 		bouy_icon.setImageResource(AppDefs.foo.get(buoy).image_resId);
 		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		sat_tv.setText(""+location.getAccuracy()+"m");
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		sat_tv.setText(""+ac.getLastLocation().getAccuracy()+"m");
+
 	}
 
 }
