1779f2ec1a573e693896f0390f71e9cf94747c4a
keyboardsurfer <keyboardsurfer@c-base.org>
2012-11-24 20:58:48 +0100

    upload of full bearings for a boy is possible

--- src/org/hackerfleet/model/Buoy.java
+++ src/org/hackerfleet/model/Buoy.java
@@ -2,6 +2,8 @@ package org.hackerfleet.model;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.util.Log;
+import org.hackerfleet.etc.AppDefs;
 import org.hackerfleet.etc.BuoyDefinition;
 import org.json.JSONArray;
 import org.json.JSONException;
