be9d686ec834ae0cbc108e4beebb78080511d3ee
ligi <ligi@ligi.de>
2012-11-24 02:52:01 +0100

    nicer code

--- src/org/hackerfleet/buoy_select/BaseBuoySelectFragment.java
+++ src/org/hackerfleet/buoy_select/BaseBuoySelectFragment.java
@@ -1,11 +1,8 @@
 package org.hackerfleet.buoy_select;
 
-import org.holoeverywhere.LayoutInflater;
 import org.holoeverywhere.app.Fragment;
 
-import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageButton;
 /**
  * a Base Fragment for Fragments that select Buoys
@@ -16,16 +13,11 @@ import android.widget.ImageButton;
 public class BaseBuoySelectFragment extends Fragment{
 
 	private BuoySelectOnClickListener mOnBtnClickListener;
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		mOnBtnClickListener=new BuoySelectOnClickListener();
-
-		return super.onCreateView(inflater, container, savedInstanceState);
-	}
-	
+		
 	public void btn2buoy(View container,int buoy_resid,String tag) {
+		if (mOnBtnClickListener==null)
+			mOnBtnClickListener=new BuoySelectOnClickListener();
+		
 		ImageButton button=(ImageButton)container.findViewById(buoy_resid);
 		button.setOnClickListener(mOnBtnClickListener);
 	}
