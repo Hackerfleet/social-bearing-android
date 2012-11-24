e4c2e5fe116090bfc19bdb5f8909ad2c7524e921
Dirk Jaeckel <dirk@jaeckel.com>
2012-11-24 10:50:53 +0100

    BearingActivity -> SimpleDaEntryActivity

--- src/org/hackerfleet/BearingActivity.java
+++ src/org/hackerfleet/BearingActivity.java
@@ -3,44 +3,26 @@ import org.holoeverywhere.app.Activity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.EditText;
 
 public class BearingActivity extends Activity {
   /**
    * Called when the activity is first created.
    */
 
-  LocationManager locationManager;
-  EditText        lat;
-  EditText        lon;
-  EditText        acc;
+  private final static String TAG = BearingActivity.class.getSimpleName();
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    super.onCreate (savedInstanceState);
-    setContentView (R.layout.buoy_select);
-    getSupportActionBar().setTitle("Corellian Engineering Corporation");
-
-    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-    lat = (EditText) findViewById(R.id.gps_lat);
-    lon = (EditText) findViewById(R.id.gps_lon);
-    acc = (EditText) findViewById(R.id.gps_acc);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.buoy_select);
+    getSupportActionBar().setTitle("BearingActivity");
 
   }
 
   @Override
   public void onResume() {
     super.onResume();
-
-    Location lastLocation = locationManager.getLastKnownLocation("GPS");
-    Log.d(TAG, "lastLocation: " + lastLocation);
-
-    if (lastLocation != null) {
-      lat.setText("" + lastLocation.getLatitude());
-      lon.setText("" + lastLocation.getLongitude());
-      acc.setText("" + lastLocation.getAccuracy());
-    }
+    Log.d(TAG, "onResume(): ");
 
   }
 }
--- src/org/hackerfleet/SimpleDataEntryActivity.java
+++ src/org/hackerfleet/SimpleDataEntryActivity.java
@@ -1,14 +1,53 @@
 package org.hackerfleet;
 
+import com.actionbarsherlock.app.SherlockActivity;
+
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 /**
  * @author flashmop
  * @date 24.11.12 09:49
  */
-public class SimpleDataEntryActivity extends Activity {
+public class SimpleDataEntryActivity extends SherlockActivity {
+  private final static String TAG = SimpleDataEntryActivity.class.getSimpleName();
+
+  LocationManager locationManager;
+  EditText        lat;
+  EditText        lon;
+  EditText        acc;
+
+  @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.simple_data_entry);
+    getSupportActionBar().setTitle("Corellian Engineering Corporation");
+
+    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+    lat = (EditText) findViewById(R.id.gps_lat);
+    lon = (EditText) findViewById(R.id.gps_lon);
+    acc = (EditText) findViewById(R.id.gps_acc);
+
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+
+    Location lastLocation = locationManager.getLastKnownLocation("GPS");
+    Log.d(TAG, "lastLocation: " + lastLocation);
+
+    if (lastLocation != null) {
+      lat.setText("" + lastLocation.getLatitude());
+      lon.setText("" + lastLocation.getLongitude());
+      acc.setText("" + lastLocation.getAccuracy());
+    }
+
   }
 }
